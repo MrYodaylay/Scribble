6ce22d2679bc6058c59bec339d305235b93b8d38
Luke Glover <53845088+Luke-Glover@users.noreply.github.com>
2022-10-11 19:16:51 +1100

    Progress on refactor: + Added combined class loader for sketches and tests + Added access widener

--- src/scribble/AccessWidener.java
+++ src/scribble/AccessWidener.java
@@ -1,6 +1,18 @@
 package scribble;
 
-public class AccessWidener {
-    //TODO
-    //copy and paste access widener thingo
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class AccessWidener extends ClassVisitor {
+
+    public AccessWidener(int opcode, ClassVisitor cv) {
+        super(opcode, cv);
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String descriptor, String signature, Object value) {
+        return super.visitField(Opcodes.ACC_PUBLIC, name, descriptor, signature, value);
+    }
+
 }
--- src/scribble/CustomClassLoader.java
+++ src/scribble/CustomClassLoader.java
@@ -1,7 +1,49 @@
 package scribble;
 
-public class CustomClassLoader extends ClassLoader{
-    //TODO
-    //Add class loading thingo and fuse sketch/test loader here
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+
+public class CustomClassLoader extends ClassLoader {
+
+    public Class<?> loadSketch(Path sketchPath) throws IOException {
+        String sketchPathStr = sketchPath.toString();
+        byte[] rawClassData = loadClassData(sketchPathStr);
+        byte[] publicClassData = widenClass(rawClassData);
+        return prepareClass(publicClassData);
+    }
+    public Class<?> loadTestSpec(Path testSpecPath) throws IOException {
+        String sketchPathStr = testSpecPath.toString();
+        byte[] rawClassData = loadClassData(sketchPathStr);
+        return prepareClass(rawClassData);
+    }
+
+    private Class<?> prepareClass(byte[] classBytes) {
+        Class<?> newClass = defineClass(null, classBytes, 0, classBytes.length);
+        resolveClass(newClass);
+        return newClass;
+    }
+
+    private byte[] loadClassData(String path) throws IOException {
+        try (InputStream fileReader = new FileInputStream(path)) {
+            return fileReader.readAllBytes();
+        }
+    }
+
+    public byte[] widenClass(byte[] classBytes) {
+        ClassWriter cw = new ClassWriter(Opcodes.ASM9);
+
+        ClassVisitor cv = new AccessWidener(Opcodes.ASM9, cw);
+
+        ClassReader cr = new ClassReader(classBytes);
+        cr.accept(cv, 0);
+        return cw.toByteArray();
+
+    }
 }
